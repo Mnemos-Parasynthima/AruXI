1a47828f1a260658cb77cd260f6a31eab669ec44
Jorge <jorgegarcia_tx@outlook.com>
2025-06-28 01:44:03 -0500

    Testing handling R instructions

--- testsuite/assembler/handleInstruction_test.go
+++ testsuite/assembler/handleInstruction_test.go
@@ -375,10 +375,220 @@ func TestHandleI(t *testing.T) {
 	arxsmDeleteSectionTable(sectTable)
 }
 
-func TestHandeR(t *testing.T) {
+func TestHandleR(t *testing.T) {
 	instrStream := arxsmInitInstrStream()
 	symbTable := arxsmInitSymbTable()
 	sectTable := arxsmInitSectionTable()
+
+	sectTable.activeSection = 2;
+
+	// Test valid instructions
+	// @0x0 add X0, X1, X2
+	t.Run("0", func(t *testing.T) {
+		instr := "add"
+		args := "X0, X1, X2"
+
+		arxsmHandleInstruction(instrStream, symbTable, sectTable, instr, args)
+
+		entry := arxsmGetInstr(instrStream, 0x0)
+		expectedInstr := "add"
+		expectedOperands := []string{"X0", "X1", "X2"}
+
+		if (entry == nil) {
+			t.Errorf("%sExpected: entry, Actual: nil%s", RED, RESET)
+		} else {
+			actualInstr := fromCharPGetString(entry.instr)
+			if (actualInstr != expectedInstr) {
+				t.Errorf("%sExpected instruction: %s, Actual instruction: %s%s", RED, expectedInstr, actualInstr, RESET)
+			} else {
+				t.Logf("%sActual instruction: %s%s", GREEN, actualInstr, RESET)
+			}
+
+			actualOperands := getStringArr(entry.operands)
+
+			if (!cmp.Equal(actualOperands, expectedOperands)) {
+				t.Errorf("%sExpected operands: %v, Actual operands: %v%s", RED, expectedOperands, actualOperands, RESET)
+			} else {
+				t.Logf("%sActual operands: %v%s", GREEN, actualOperands, RESET)
+			}
+		}
+	})
+
+	sectTable.entries[3].lp += 4
+
+	// @0x4 not X5, 	 X10
+	t.Run("1", func(t *testing.T) {
+		instr := "not"
+		args := "X5, 	 X10"
+
+		arxsmHandleInstruction(instrStream, symbTable, sectTable, instr, args)
+
+		entry := arxsmGetInstr(instrStream, 0x4)
+		expectedInstr := "not"
+		expectedOperands := []string{"X5", "X10"}
+
+		if (entry == nil) {
+			t.Errorf("%sExpected: entry, Actual: nil%s", RED, RESET)
+		} else {
+			actualInstr := fromCharPGetString(entry.instr)
+			if (actualInstr != expectedInstr) {
+				t.Errorf("%sExpected instruction: %s, Actual instruction: %s%s", RED, expectedInstr, actualInstr, RESET)
+			} else {
+				t.Logf("%sActual instruction: %s%s", GREEN, actualInstr, RESET)
+			}
+
+			actualOperands := getStringArr(entry.operands)
+
+			if (!cmp.Equal(actualOperands, expectedOperands)) {
+				t.Errorf("%sExpected operands: %v, Actual operands: %v%s", RED, expectedOperands, actualOperands, RESET)
+			} else {
+				t.Logf("%sActual operands: %v%s", GREEN, actualOperands, RESET)
+			}
+		}
+	})
+
+	sectTable.entries[3].lp += 4
+
+	// @0x8 cmp   X7, x4
+	t.Run("2", func(t *testing.T) {
+		instr := "cmp"
+		args := "X7, x4"
+
+		arxsmHandleInstruction(instrStream, symbTable, sectTable, instr, args)
+
+		entry := arxsmGetInstr(instrStream, 0x8)
+		expectedInstr := "cmp"
+		expectedOperands := []string{"xz", "X7", "x4"}
+
+		if (entry == nil) {
+			t.Errorf("%sExpected: entry, Actual: nil%s", RED, RESET)
+		} else {
+			actualInstr := fromCharPGetString(entry.instr)
+			if (actualInstr != expectedInstr) {
+				t.Errorf("%sExpected instruction: %s, Actual instruction: %s%s", RED, expectedInstr, actualInstr, RESET)
+			} else {
+				t.Logf("%sActual instruction: %s%s", GREEN, actualInstr, RESET)
+			}
+
+			actualOperands := getStringArr(entry.operands)
+
+			if (!cmp.Equal(actualOperands, expectedOperands)) {
+				t.Errorf("%sExpected operands: %v, Actual operands: %v%s", RED, expectedOperands, actualOperands, RESET)
+			} else {
+				t.Logf("%sActual operands: %v%s", GREEN, actualOperands, RESET)
+			}
+		}
+	})
+
+	sectTable.entries[3].lp += 4
+
+	// @0xc mv X10  , xr
+	t.Run("3", func(t *testing.T) {
+		instr := "mv"
+		args := "X10  , xr"
+
+		arxsmHandleInstruction(instrStream, symbTable, sectTable, instr, args)
+
+		entry := arxsmGetInstr(instrStream, 0xc)
+		expectedInstr := "mv"
+		expectedOperands := []string{"X10", "xr"}
+
+		if (entry == nil) {
+			t.Errorf("%sExpected: entry, Actual: nil%s", RED, RESET)
+		} else {
+			actualInstr := fromCharPGetString(entry.instr)
+			if (actualInstr != expectedInstr) {
+				t.Errorf("%sExpected instruction: %s, Actual instruction: %s%s", RED, expectedInstr, actualInstr, RESET)
+			} else {
+				t.Logf("%sActual instruction: %s%s", GREEN, actualInstr, RESET)
+			}
+
+			actualOperands := getStringArr(entry.operands)
+
+			if (!cmp.Equal(actualOperands, expectedOperands)) {
+				t.Errorf("%sExpected operands: %v, Actual operands: %v%s", RED, expectedOperands, actualOperands, RESET)
+			} else {
+				t.Logf("%sActual operands: %v%s", GREEN, actualOperands, RESET)
+			}
+		}
+	})
+
+	sectTable.entries[3].lp += 4
+
+	// @0x10 mvn X20	 , x0
+	t.Run("4", func(t *testing.T) {
+		instr := "mvn"
+		args := "X20\t , x0"
+
+		arxsmHandleInstruction(instrStream, symbTable, sectTable, instr, args)
+
+		entry := arxsmGetInstr(instrStream, 0x10)
+		expectedInstr := "mvn"
+		expectedOperands := []string{"X20", "x0"}
+
+		if (entry == nil) {
+			t.Errorf("%sExpected: entry, Actual: nil%s", RED, RESET)
+		} else {
+			actualInstr := fromCharPGetString(entry.instr)
+			if (actualInstr != expectedInstr) {
+				t.Errorf("%sExpected instruction: %s, Actual instruction: %s%s", RED, expectedInstr, actualInstr, RESET)
+			} else {
+				t.Logf("%sActual instruction: %s%s", GREEN, actualInstr, RESET)
+			}
+
+			actualOperands := getStringArr(entry.operands)
+
+			if (!cmp.Equal(actualOperands, expectedOperands)) {
+				t.Errorf("%sExpected operands: %v, Actual operands: %v%s", RED, expectedOperands, actualOperands, RESET)
+			} else {
+				t.Logf("%sActual operands: %v%s", GREEN, actualOperands, RESET)
+			}
+		}
+	})
+
+	// Test invalid instructions
+	// Note that invalid instructions are limited, see comment in `handleIR`
+	// @0x10 mvn X4
+	t.Run("5", func(t *testing.T) {
+		if (os.Getenv("FORK") == "1") {
+			instr := "mvn"
+			args := "x4"
+			
+			arxsmHandleInstruction(instrStream, symbTable, sectTable, instr, args)
+			os.Stderr.WriteString("UNREACHABLE: Instruction added")
+			os.Exit(1)
+		}
+
+		stdout, stderr, err := runFork(t, "HandleR/5")
+		_ = stdout
+
+		if (err.Error() == "exit status 255" && stderr == "\x1b[31mINVALID SYNTAX ERROR: Expected operands\n\x1b[0m") {
+			t.Logf("%sGot %s with error %s%s", GREEN, err.Error(), stderr, RESET)
+		} else {
+			t.Errorf("%sExpected Invalid Syntax Error, got %s%s", RED, stderr, RESET)
+		}
+	})
+
+	// @0x10 add X4. x10, x2, x4
+	t.Run("6", func(t *testing.T) {
+		if (os.Getenv("FORK") == "1") {
+			instr := "add"
+			args := "X4, x10, x2, x4"
+			
+			arxsmHandleInstruction(instrStream, symbTable, sectTable, instr, args)
+			os.Stderr.WriteString("UNREACHABLE: Instruction added")
+			os.Exit(1)
+		}
+
+		stdout, stderr, err := runFork(t, "HandleR/6")
+		_ = stdout
+
+		if (err.Error() == "exit status 255" && stderr == "\x1b[31mINVALID SYNTAX ERROR: Unexpected operands: `x2`\n\x1b[0m") {
+			t.Logf("%sGot %s with error %s%s", GREEN, err.Error(), stderr, RESET)
+		} else {
+			t.Errorf("%sExpected Invalid Syntax Error, got %s%s", RED, stderr, RESET)
+		}
+	})
 
 	arxsmDeleteInstrStream(instrStream)
 	arxsmDeleteSymbTable(symbTable)
